0002b91ff11253bb62d522c005a6f685f24f3e16
Zerokyh <109337472+Zerokyh@users.noreply.github.com>
2024-06-22 21:55:14 +0900

    Delete src/SNSText.tsx

--- src/SNSText.tsx
+++
@@ -1,9 +0,0 @@
-export type SNSTextProps = {
-  sns: "Facebook" | "Google" | "Apple";
-};
-
-const SNSText = ({ sns }: SNSTextProps) => {
-  return <span>Continue with {sns}</span>;
-};
-
-export default SNSText;
